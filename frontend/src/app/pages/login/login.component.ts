ef40dcba73b1404101cd538e6e3f5e7d8b2b983b
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-11-22 17:57:07 +0100

    create login and register component

---
+++ frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    this.userService.login(this.loginForm.value).subscribe({
+      next: () => this.router.navigate(['/home']),
+      error: (err) => (this.errorMessage = err.error.message || 'Login failed'),
+    });
+  }
+}
---
+++ frontend/src/app/pages/register/register.component.ts
@@ -0,0 +1,50 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+  errorMessage: string = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.invalid) return;
+
+    const { confirmPassword, ...userData } = this.registerForm.value;
+    if (userData.password !== confirmPassword) {
+      this.errorMessage = 'Passwords do not match';
+      return;
+    }
+
+    this.userService.register(userData).subscribe({
+      next: () => this.router.navigate(['/login']),
+      error: (err) =>
+        (this.errorMessage = err.error.message || 'Registration failed'),
+    });
+  }
+}
