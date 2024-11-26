100d89476a40f39b36036fc1ad513b3a105e9ec1
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-11-26 19:55:06 +0100

    Create Product Item Component

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ProductListComponent } from '../../components/product-list/product-list.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule, CommonModule, ProductListComponent],
+  imports: [RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
--- frontend/src/app/pages/shop/shop.component.ts
+++ frontend/src/app/pages/shop/shop.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ShopService } from '../../core/services/shop.service';
 import { Product } from '../../models/product.model';
+import { ProductItemComponent } from "../../components/product-item/product-item.component";
 
 @Component({
   selector: 'app-shop',
-  imports: [],
+  imports: [ProductItemComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css',
 })
