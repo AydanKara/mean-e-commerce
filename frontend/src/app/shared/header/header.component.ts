1e008a78b75d6ccf8e0d4694d41c75965fca2f09
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-12-12 21:44:31 +0100

    update header component

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -1,21 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { UserService } from '../../core/services/user.service';
 import { Router, RouterModule } from '@angular/router';
 import { User } from '../../models/user.model';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, CommonModule, MatProgressBarModule],
+  imports: [RouterModule, CommonModule, MatProgressBarModule, MatIcon],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  private userService = inject(UserService);
+  private router = inject(Router);
+
   currentUser: User | null = null;
   isLoading: boolean = true;
-
-  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
     this.userService.getLoadingState().subscribe((loading) => {
@@ -28,6 +30,26 @@ export class HeaderComponent {
       this.userService.setLoadingState(false); // Stop loading when auth state is resolved
     });
     console.log(this.isLoading);
+  }
+
+  navigateToAccount() {
+    if (this.currentUser) {
+      // If user is logged in, navigate to account page
+      this.router.navigate(['/account']);
+    } else {
+      // If no user is logged in, navigate to login page
+      this.router.navigate(['/login']);
+    }
+  }
+
+  navigateToWishlist() {
+    if (this.currentUser) {
+      // If user is logged in, navigate to account page
+      this.router.navigate(['/account-wishlist']);
+    } else {
+      // If no user is logged in, navigate to login page
+      this.router.navigate(['/login']);
+    }
   }
 
   onLogout(): void {
