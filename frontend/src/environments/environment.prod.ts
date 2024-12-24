97b4348153ea39ef408112f4e111da454bd28aac
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-12-24 18:09:39 +0100

    update vercel.json

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:
-    process.env[
-      'NG_APP_API_URL'
-    ] /* 'https://mean-e-commerce.onrender.com/api', */,
+  apiUrl: '', // Placeholder
 };
