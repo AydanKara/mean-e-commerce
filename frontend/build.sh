fee345072fbd5da257ad481a0ddce47e5fae5c52
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-12-25 10:55:47 +0100

    delete vercel.json

--- frontend/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Replace the placeholder in environment.prod.ts with the actual value of NG_APP_API_URL
-sed -i "s/\${NG_APP_API_URL}/$NG_APP_API_URL/" ./src/environments/environment.prod.ts
-
-# Run the Angular production build
-ng build --configuration production
