78a36e61985af0f92aaa7ba939633aa4dcb88b59
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-12-26 21:26:59 +0100

    Auto-generated commit

--- chunk-2IFWVJDF.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./chunk-HXLDE4VL.js";import{pb as i}from"./chunk-XVGCL5GT.js";var m=class e{transform(r,t="dd MMM, yyyy",a="en-US",o){return r?n(r,t,a,o):""}static \u0275fac=function(t){return new(t||e)};static \u0275pipe=i({name:"formatDate",type:e,pure:!0})};export{m as a};
