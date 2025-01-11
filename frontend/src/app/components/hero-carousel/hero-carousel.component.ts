d31e6e2f2f829ddbefac5e50d9ddd651133553bf
AydanKara <113279073+AydanKara@users.noreply.github.com>
2025-01-11 01:48:07 +0100

    create hero carousel component
    delete old carousel component and implement new carousel component with bootstrap

---
+++ frontend/src/app/components/hero-carousel/hero-carousel.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-carousel',
+  imports: [],
+  templateUrl: './hero-carousel.component.html',
+  styleUrl: './hero-carousel.component.css',
+})
+export class HeroCarouselComponent {}
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,21 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FeaturesComponent } from '../../shared/features/features.component';
-import { CarouselComponent } from '../../components/carousel/carousel.component';
 import { CategoriesComponent } from '../../components/categories/categories.component';
 import { CategoryService } from '../../core/services/category.service';
 import { Category } from '../../models/category.model';
 import { MustHavesComponent } from '../../components/must-haves/must-haves.component';
-import { animate, style, transition, trigger } from '@angular/animations';
 import { ShopService } from '../../core/services/shop.service';
 import { Product } from '../../models/product.model';
-import {
-  sliderHeroAnimation,
-  sliderNewProductsAnimation,
-} from '../../animations/animations';
 import { NewProductsCarouselComponent } from '../../components/new-products-carousel/new-products-carousel.component';
 import { BlogsComponent } from '../../components/blogs/blogs.component';
+import { HeroCarouselComponent } from '../../components/hero-carousel/hero-carousel.component';
 
 @Component({
   selector: 'app-home',
@@ -23,15 +18,14 @@ import { BlogsComponent } from '../../components/blogs/blogs.component';
     RouterModule,
     CommonModule,
     FeaturesComponent,
-    CarouselComponent,
     CategoriesComponent,
     MustHavesComponent,
     NewProductsCarouselComponent,
     BlogsComponent,
+    HeroCarouselComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  animations: [sliderHeroAnimation, sliderNewProductsAnimation],
 })
 export class HomeComponent implements OnInit {
   private categoryService = inject(CategoryService);
@@ -41,78 +35,6 @@ export class HomeComponent implements OnInit {
   brands: string[] = [];
   errorMessage: string = '';
   loading: boolean = false;
-
-  slides = [
-    {
-      image: 'assets/cover-3.jpg',
-      content: `
-        <div class="container d-flex flex-column">
-            <div class="row justify-content-end" style="min-height: 550px;">
-              <div class="col-12 col-md-6 col-lg-5 col-xl-4 mx-lg-5 text-md-black text-sm-center text-lg-end pb-5">
-
-                <!-- Heading -->
-                <h1 class="mb-4">Summer Collection</h1>
-
-                <!-- Text -->
-                <p class="mb-5 fs-5 text-gray">
-                  So called give, one whales tree seas dry place own
-                  day, winged tree created spirit.
-                </p>
-
-                <!-- Button -->
-                <a class="button-nav-dark" href="/shop">
-                  Shop Now
-                </a>
-
-              </div>
-            </div>
-          </div>
-      `,
-    },
-    {
-      image: 'assets/cover-tudor.jpg',
-      content: `
-        <div class='container d-flex flex-column'>
-          <div class='row align-items-center py-5' style='min-height: 550px;'>
-            <div class='col-12 col-md-6 col-lg-5 col-xl-4 text-black '>
-              <h1>Summer Sale</h1>
-              <h2 class='display-1 fs-lg fw-bold text-primary'>-70%</h2>
-              <h5 class='mt-n4 mb-5'>with promo code CN67EW*</h5>
-              <a class='button-nav-dark' href='/shop'>Shop Now</a>
-            </div>
-          </div>
-        </div>
-      `,
-    },
-    {
-      image: 'assets/cover-chanel.jpg',
-      content: `
-        <div class="bg-dark-20">
-            <div class="container d-flex flex-column">
-              <div class="row align-items-center justify-content-end" style="min-height: 550px;">
-                <div class="col-12 text-center text-white">
-
-                  <!-- Preheading -->
-                  <h4 class="mb-0">Summer Styles</h4>
-
-                  <!-- Heading -->
-                  <h1 class="display-1">
-                    50% OFF
-                  </h1>
-
-                  <!-- Links -->
-                  <p class="mb-0">
-                    <a href="/shop" class="link-underline text-reset mx-5 my-5">Shop Women</a>
-                    <a href="/shop" class="link-underline text-reset mx-5 my-5">Shop Men</a>
-                  </p>
-
-                </div>
-              </div>
-            </div>
-          </div>
-      `,
-    },
-  ];
 
   ngOnInit(): void {
     this.categoryService.getAllCategories();
